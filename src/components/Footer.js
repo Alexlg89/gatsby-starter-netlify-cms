6e639c4a061622c594689c19ceeb80a7d608e88c
Alex Elert <alex@Daniels-MacBook.fritz.box>
2019-01-11 08:22:55 +0100

    add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="content has-text-centered">2019 - Testfooter</div>
+    </div>
+  );
+};
+
+export default Footer;
